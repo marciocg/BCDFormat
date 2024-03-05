7a18d6191388cba22efec0c19b7cde21ade8eeff
Márcio C.G. <marcio.goulart@gmail.com>
2024-03-05 18:31:45 -0300

    v0.5 More tests for better code coverage

--- src/test/java/io/github/marciocg/BCDFormatTest.java
+++ src/test/java/io/github/marciocg/BCDFormatTest.java
@@ -9,40 +9,48 @@ import static io.github.marciocg.BCDFormat.fromStringToBCDByteArray;
 /**
  * Unit test for BCDFormat static methods
  */
-public class BCDFormatTest 
-{
+public class BCDFormatTest {
+
     /**
      * Test decoding from BCD data as byte[] array to String, and vice-versa
      */
     @Test
-    public void fromBCDtoStringAndBackTest()
-    {
+    public void fromBCDtoStringAndBackTest() {
         String input = "498455910123456789abcdefABCDEFa0eeccdd858fd9d0d1eaaecafebE";
-        byte [] result = fromStringToBCDByteArray(input);
+        byte[] result = fromStringToBCDByteArray(input);
         String decoded = fromBCDByteArrayToString(result);
 
         assertEquals(input.toLowerCase(), decoded);
     }
 
     /**
-     * Test with invalid hexadecimal character on String input, expects IllegalArgumentException
+     * Test with invalid hexadecimal character on second position of a pair from String input, expects IllegalArgumentException
+     * 
      */
     @Test(expected = IllegalArgumentException.class)
-    public void fromStringWithInvalidHexChar()
-    {
+    public void fromStringWithInvalidHexChar2() {
         String input = "498455910123456789abcdefABCDEFa0eeccdd858fd9d0d1eaaecafebW";
-        byte [] result = fromStringToBCDByteArray(input);
+        byte[] result = fromStringToBCDByteArray(input);
         String decoded = fromBCDByteArrayToString(result);
     }
 
-        /**
+    /**
+     * Test with invalid hexadecimal character on first position of a pair from String input, expects IllegalArgumentException
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void fromStringWithInvalidHexChar1() {
+        String input = "498455910123456789abcdefABCDEFa0eeccdd858fd9d0d1eaaecafeWb";
+        byte[] result = fromStringToBCDByteArray(input);
+        String decoded = fromBCDByteArrayToString(result);
+    }
+
+    /**
      * Test with odd amount of digits, expects ArrayIndexOutOfBoundsException
      */
     @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void fromStringWithOddDigitsLength()
-    {
+    public void fromStringWithOddDigitsLength() {
         String input = "498455910123456789abcdefABCDEFa0eeccdd858fd9d0d1eaaecafeb";
-        byte [] result = fromStringToBCDByteArray(input);
+        byte[] result = fromStringToBCDByteArray(input);
         String decoded = fromBCDByteArrayToString(result);
     }
 
